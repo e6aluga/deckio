d2d6188f9e3f4a3bdbb4a4fb00bedefdcecfef07
Yury Hlynovsky <106775000+Yuriy.Khlynovskiy@incomsystem.ru>
2024-08-23 10:06:57 +0300

    Game pcLocation sdLocation checkSdStatus fixed

--- src/main/java/com/example/dddeck/Game.java
+++ src/main/java/com/example/dddeck/Game.java
@@ -1,30 +1,48 @@
 package com.example.dddeck;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 
 public class Game {
 
     private String game;
+    private String pcLocation;
+    private String sdLocation;
+    private String host;
+    private String user;
+    private String password;
 
     @FXML
     private Label gameNameLabel;
 
+    @FXML
+    private Label pcLocationLabel;
+
+    @FXML
+    private Label sdLocationLabel;
+
+    @FXML
+    private Label sdStatusLabel;
+
     public Game() {
-        // Конструктор по умолчанию. Здесь не нужно ничего передавать.
+        // Конструктор по умолчанию
     }
 
-    // Этот метод будет вызван после загрузки FXML
     @FXML
     public void setName(String game) {
-        System.out.println("game is: " + game);
         this.game = game;
         if (gameNameLabel != null) {
             gameNameLabel.setText(game);  // Устанавливаем текст в метку
@@ -33,7 +51,7 @@ public class Game {
 
     @FXML
     public void initialize() {
-        // Здесь можно вызвать другие начальные действия, если нужно
+        // Метод инициализации вызывается после загрузки FXML
     }
 
     public void init(String name) {
@@ -45,6 +63,12 @@ public class Game {
             Game controller = loader.getController();
             controller.setName(name);  // Передаём имя игры в контроллер
 
+            // Загружаем конфигурацию игры после установки имени
+            controller.loadGameConfig(name);
+            controller.loadSteamDeckSettings();
+            controller.updateLabels();
+            controller.checkSdStatus();
+
             Stage stage = new Stage();
             stage.setTitle(name);
             stage.setScene(new Scene(root, 700, 300));
@@ -53,5 +77,75 @@ public class Game {
             e.printStackTrace();
         }
     }
+
+    private void loadGameConfig(String name) {
+        System.out.println(App.timestamp() + "loadGameConfig()");
+        Gson gson = new Gson();
+        String filepath = String.format("configs/%s", name);
+        Map<String, String> map = null;
+
+        try (FileReader fileReader = new FileReader(filepath)) {
+            Type type = new TypeToken<Map<String, String>>() {}.getType();
+            map = gson.fromJson(fileReader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(App.timestamp() + "Game settings " + map);
+        if (map != null) {
+            this.pcLocation = map.get("saveLocationPC");
+            this.sdLocation = map.get("saveLocationSteamDeck");
+        }
+    }
+
+    private void loadSteamDeckSettings(){
+        System.out.println(App.timestamp() + "loadSteamDeckSettings()");
+        Gson gson = new Gson();
+        String filepath = "settings.json";
+        Map<String, String> map = null;
+
+        try (FileReader fileReader = new FileReader(filepath)) {
+            Type type = new TypeToken<Map<String, String>>() {}.getType();
+            map = gson.fromJson(fileReader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(App.timestamp() + "Steam Deck Settings: " + map);
+
+        if (map != null) {
+            this.host = map.get("deckIp");
+            this.user = map.get("deckUser");
+            this.password = map.get("deckPassword");
+        }
+    }
+
+    private void updateLabels() {
+        if (pcLocation != null) {
+            pcLocationLabel.setText(pcLocation);
+        }
+        if (sdLocation != null) {
+            sdLocationLabel.setText(sdLocation);
+        }
+    }
+
+    private boolean checkSdStatus(){
+        SSHManager sshManager = new SSHManager();
+        String status = sshManager.sshExec(this.host, this.user, this.password, "22", "date");
+        if (status != null){
+            System.out.println(App.timestamp() + "checkSdStatus: successfull");
+            sdStatusLabel.setText("Steam Deck status: connected!");
+            return true;
+            
+        } else {
+            System.out.println(App.timestamp() + "checkSdStatus: error");
+            sdStatusLabel.setText("Steam Deck status: error!");
+            return false;
+        }
+    }
 }
+
+
+
+    
+
 
